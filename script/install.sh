a0ba4046fdc690bd6acf624f1a45db7891e5e35e
Akshay <akshaycm@hotmail.com>
2018-09-29 23:13:10 +0300

    colors and some stuff

--- script/install.sh
+++ script/install.sh
@@ -25,7 +25,7 @@ server {
 
     root /var/www/html;
     index index.html index.htm index.nginx-debian.html;
-    #server_name explorer.bulwarkcrypto.com;
+    #server_name explorer.dogecashcrypto.com;
     server_name _;
 
     gzip on;
@@ -51,21 +51,21 @@ server {
 
     #listen [::]:443 ssl ipv6only=on; # managed by Certbot
     #listen 443 ssl; # managed by Certbot
-    #ssl_certificate /etc/letsencrypt/live/explorer.bulwarkcrypto.com/fullchain.pem; # managed by Certbot
-    #ssl_certificate_key /etc/letsencrypt/live/explorer.bulwarkcrypto.com/privkey.pem; # managed by Certbot
+    #ssl_certificate /etc/letsencrypt/live/explorer.dogecashcrypto.com/fullchain.pem; # managed by Certbot
+    #ssl_certificate_key /etc/letsencrypt/live/explorer.dogecashcrypto.com/privkey.pem; # managed by Certbot
     #include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
     #ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
 }
 
 #server {
-#    if ($host = explorer.bulwarkcrypto.com) {
+#    if ($host = explorer.dogecashcrypto.com) {
 #        return 301 https://\$host\$request_uri;
 #    } # managed by Certbot
 #
 #	listen 80 default_server;
 #	listen [::]:80 default_server;
 #
-#	server_name explorer.bulwarkcrypto.com;
+#	server_name explorer.dogecashcrypto.com;
 #   return 404; # managed by Certbot
 #}
 EOL
@@ -89,37 +89,37 @@ installMongo () {
 
 installDogeCash () {
     echo "Installing DogeCash..."
-    mkdir -p /tmp/bulwark
-    cd /tmp/bulwark
-    curl -Lo bulwark.tar.gz $bwklink
-    tar -xzf bulwark.tar.gz
+    mkdir -p /tmp/dogecash
+    cd /tmp/dogecash
+    curl -Lo dogecash.tar.gz $bwklink
+    tar -xzf dogecash.tar.gz
     sudo mv ./bin/* /usr/local/bin
     cd
-    rm -rf /tmp/bulwark
-    mkdir -p /home/explorer/.bulwark
-    cat > /home/explorer/.bulwark/bulwark.conf << EOL
+    rm -rf /tmp/dogecash
+    mkdir -p /home/explorer/.dogecash
+    cat > /home/explorer/.dogecash/dogecash.conf << EOL
 rpcport=52544
 rpcuser=$rpcuser
 rpcpassword=$rpcpassword
 daemon=1
 txindex=1
 EOL
-    sudo cat > /etc/systemd/system/bulwarkd.service << EOL
+    sudo cat > /etc/systemd/system/dogecashd.service << EOL
 [Unit]
-Description=bulwarkd
+Description=dogecashd
 After=network.target
 [Service]
 Type=forking
 User=explorer
 WorkingDirectory=/home/explorer
-ExecStart=/home/explorer/bin/bulwarkd -datadir=/home/explorer/.bulwark
-ExecStop=/home/explorer/bin/bulwark-cli -datadir=/home/explorer/.bulwark stop
+ExecStart=/home/explorer/bin/dogecashd -datadir=/home/explorer/.dogecash
+ExecStop=/home/explorer/bin/dogecash-cli -datadir=/home/explorer/.dogecash stop
 Restart=on-abort
 [Install]
 WantedBy=multi-user.target
 EOL
-    sudo systemctl start bulwarkd
-    sudo systemctl enable bulwarkd
+    sudo systemctl start dogecashd
+    sudo systemctl enable dogecashd
     echo "Sleeping for 1 hour while node syncs blockchain..."
     sleep 1h
     clear
@@ -127,20 +127,20 @@ EOL
 
 installBlockEx () {
     echo "Installing BlockEx..."
-    git clone https://github.com/bulwark-crypto/bulwark-explorer.git /home/explorer/blockex
+    git clone https://github.com/dogecash-crypto/dogecash-explorer.git /home/explorer/blockex
     cd /home/explorer/blockex
     yarn install
     cat > /home/explorer/blockex/config.js << EOL
 const config = {
   'api': {
-    'host': 'https://explorer.bulwarkcrypto.com',
+    'host': 'https://explorer.dogecashcrypto.com',
     'port': '3000',
     'prefix': '/api',
     'timeout': '180s'
   },
   'coinMarketCap': {
     'api': 'http://api.coinmarketcap.com/v1/ticker/',
-    'ticker': 'bulwark'
+    'ticker': 'dogecash'
   },
   'db': {
     'host': '127.0.0.1',
@@ -190,7 +190,7 @@ clear
 
 # Variables
 echo "Setting up variables..."
-bwklink=`curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
+bwklink=`curl -s https://api.github.com/repos/dogecash-crypto/dogecash/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4`
 rpcuser=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 rpcpassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
 echo "Repo: $bwklink"
--- server/maintenance.js
+++ server/maintenance.js
@@ -81,15 +81,15 @@ a {
   </head>
   <body>
     <div class="logo">
-      <a href="https://bulwarkcrypto.com/">
-        <img src="https://bulwarkcrypto.com/wp-content/uploads/2018/06/logo@2x.png" title="DogeCash Crypto"/>
+      <a href="https://dogecashcrypto.com/">
+        <img src="https://dogecashcrypto.com/wp-content/uploads/2018/06/logo@2x.png" title="DogeCash Crypto"/>
       </a>
       </div>
     <div class="container">
         <img src="https://media.giphy.com/media/vIDsZm5DMZ5Ha/giphy.gif" title="Sad Explorer."/>
       <h1>Maintenance</h1>
       <p>DogeCash Explorer is not available at this moment.<br/>Please check back later.</p><br/>
-  <a href="https://bulwarkcrypto.com/" class="button">Back to Main</a>
+  <a href="https://dogecashcrypto.com/" class="button">Back to Main</a>
       </div>
   </body>
 </html>
